2304133043f427dd585967eaa8b5bb170a614522
Dina Muktubayeva <58412946+DinaMuktubayeva@users.noreply.github.com>
2020-12-06 00:11:29 +0600

    Update main.cpp

--- queue (linked list)/main.cpp
+++ queue (linked list)/main.cpp
@@ -5,51 +5,55 @@ void output(const queue& q) {
 	std::cout << q.size() << " " << q.empty() << " " << q << std::endl;
 }
 
-// This is a minimal testing file.... we will use something more complete
-// for grading!!!
 int main() {
-
+	std::cout << "A queue\n";
 	queue q1;
 	output(q1);
 
+	std::cout << "Empty queue\n";
 	queue q2 = {};
 	output(q2);
 
+	std::cout << "Initialized copy\n";
 	queue q3(q2);
+	output(q3);
 
-	for (int i = 1; i < 10; ++i) {
-		q2.push(i);
-	}
-
+	std::cout << "Self-assignment\n";
 	queue q;
 	output(q);
 	q = q;
-	std::cout << "Self-assignment\n";
 	output(q);
-	output(q2); output(q3);
-	q1 = q2;
+	
+	std::cout << "Pushing elements into a queue\n";
+	for (int i = 1; i < 10; ++i)
+		q2.push(i);
+		
+	output(q2);
 
+
+
+	std::cout << "Popping from a queue\n";
 	for (int x = 0; x < 10; ++x) {
 		std::cout << q2.peek() << " ";
 		q2.pop();
 	}
 	std::cout << std::endl;
+	
+	std::cout << "Copying queue's content\n";
+	q1 = q2;
+	output(q2);
 
-	output(q1); output(q2);
-
+	std::cout << "Clearing a queue\n";
 	q1.clear();
 
-	output(q1); output(q2);
+	output(q1); 
 
+	std::cout << "Popping from a queue until it's empty\n";
 	while (!q2.empty()) {
 		std::cout << q2.peek() << " ";
 		q2.pop();
 	}
-
+	
 	std::cout << std::endl;
-
 	output(q2);
 }
-
-
-
