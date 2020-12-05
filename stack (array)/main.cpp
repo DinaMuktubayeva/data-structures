a967947276ddfef9aa8980e0d1162219611cca22
Dina Muktubayeva <58412946+DinaMuktubayeva@users.noreply.github.com>
2020-12-06 00:37:28 +0600

    Update main.cpp

--- stack (array)/main.cpp
+++ stack (array)/main.cpp
@@ -7,47 +7,49 @@ void output(stack st) {
 	std::cout << "" << std::endl;
 }
 
-// This is a minimal testing file.... we will use something more complete
-// for grading!!!
 int main() {
 
+	std::cout << "Initializing a stack\n" << std::endl;
 	stack st1;
 	output(st1);
 
-	for (int i = 11; i < 100; i = i + 11) {
+	std::cout << "Pushing items into the stack\n" << std::endl;
+	for (int i = 11; i < 100; i = i + 11)
 		st1.push(i);
-	}
+	
 	output(st1);
 
-	for (int i = 100; i < 1000; i = i + 100) {
+	std::cout << "Pushing more items into the stack\n" << std::endl;
+	for (int i = 100; i < 1000; i = i + 100)
 		st1.push(i);
-	}
+	
 	output(st1);
-
+	
+	std::cout << "Initializing a stack as a copy\n" << std::endl;
 	stack st2 = st1;
-	stack st3 = {123, 234, 345, 456, 567, 678, 789};
-	stack st4(st3);
-
 	output(st2);
+	
+	std::cout << "Declaring a stack with initial values\n" << std::endl;	
+	stack st3 = {123, 234, 345, 456, 567, 678, 789};
 	output(st3);
-	output(st4);
-
+	
+	std::cout << "Initializing a stack as a copy\n" << std::endl;
+	stack st4(st3);
+	output(st4);	
+	
+	std::cout << "Clearing a stack\n" << std::endl;
 	st1.clear();
-
+	output(st1);
+	
+	std::cout << "Peek, push and pop\n" << std::endl;
 	for (int i = 0; i < 5; i++) {
 		std::cout << st2.peek() << " ";
 		st3.push(st2.peek());
 		st2.pop();
 	}
 	std::cout << std::endl;
-
-	output(st1);
+	
 	output(st2);
 	output(st3);
 	output(st4);
-
-	stack st;
 }
-
-
-
